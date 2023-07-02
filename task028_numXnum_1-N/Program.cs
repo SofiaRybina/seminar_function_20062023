eaf79667a0330abcb3d0549dfa6016f3750be97c
Sofia <rybina.sofia@gmail.com>
2023-07-02 11:10:11 +0300

    Произведение чисел от 1 до N

---
+++ task028_numXnum_1-N/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int ProductNum(int num)
+{
+    int res = 1;    
+    for(int i = 1; i < num; i++)
+    {
+        res = res * (i + 1);
+        Console.WriteLine($"res{i}->{res}");
+    }
+    return res;
+}
+
+int number = Prompt("Enter number N: ");
+int result = ProductNum(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} -> {result}");
