d98306bddfd0d6139fa055461211e68b77c112c9
SofiaRybina <135506751+SofiaRybina@users.noreply.github.com>
2023-08-01 16:47:06 +0300

    Произведение чисел от 1 до N. Два решения, с checked типа int

--- task028_numXnum_1-N/Program.cs
+++ task028_numXnum_1-N/Program.cs
@@ -25,3 +25,24 @@ int ProductNum(int num)
 int number = Prompt("Enter number N: ");
 int result = ProductNum(number);
 Console.WriteLine($"Произведение чисел от 1 до {number} -> {result}");
+
+
+//Альтернативное решение, с проверкой на переполнение типа int
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} -> {factorial}");
+
+int Factorial(int num)
+{
+    int fac = 1;    
+    for(int i = 1; i <= num; i++)
+    {
+        checked     //проверка на переполняемость типа int, будет выпадать ошибка,если будет переполнен 
+        {
+            fac *= i;
+        }
+    }
+    return fac;
+}
